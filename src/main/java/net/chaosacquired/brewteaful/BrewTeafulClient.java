b9611e237ce1221b391089396c8587cdcaf24870
BuildTools <unconfigured@null.spigotmc.org>
2024-09-07 14:16:44 -0700

    1

---
+++ src/main/java/net/chaosacquired/brewteaful/BrewTeafulClient.java
@@ -0,0 +1,10 @@
+package net.chaosacquired.brewteaful;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BrewTeafulClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/chaosacquired/brewteaful/BrewTeafulDataGenerator.java
@@ -0,0 +1,11 @@
+package net.chaosacquired.brewteaful;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BrewTeafulDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
